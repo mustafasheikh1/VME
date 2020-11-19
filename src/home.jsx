004fad736dfdee85807e5a5906eef673ec66eb93
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-11-20 04:43:57 +0500

    Banner created

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import "./css/home.css";
+import "./css/cards.css";
+import carousel from "./dodge2.jpg";
 class Home extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>Home</h1> );
-    }
+  state = {
+    carouselImage: carousel,
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className="banner">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="carousel-caption d-md-block">
+                <div className="details">
+                  <h1>Welcome to Vehicle Metadata Extraction!</h1>
+                  <p>Get started With us today.</p>
+                </div>
+              </div>
+
+              <img
+                src={this.state.carouselImage}
+                className="d-block w-100"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Home;
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,39 +1,42 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
-    return ( <React.Fragment>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <Link class="navbar-brand" href="#">Navbar</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <Link class="nav-link" href="#">Home <span class="sr-only">(current)</span></Link>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link" href="#">Link</Link>
-      </li>
-      <li class="nav-item dropdown">
-        <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+          VME
         </Link>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <Link class="dropdown-item" href="#">Action</Link>
-          <Link class="dropdown-item" href="#">Another action</Link>
-          <div class="dropdown-divider"></div>
-          <Link class="dropdown-item" href="#">Something else here</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link active" to="/">
+              Home <span className="sr-only">(current)</span>
+            </NavLink>
+            <NavLink className="nav-link" to="/user">
+              Upload Picture
+            </NavLink>
+            <NavLink className="nav-link" to="/about">
+              About Us
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact Us
+            </NavLink>
+          </div>
         </div>
-      </li>
-      <li class="nav-item">
-        <Link class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
-    </React.Fragment> );
-}
- 
+      </nav>
+    </React.Fragment>
+  );
+};
+
 export default Navbar;
