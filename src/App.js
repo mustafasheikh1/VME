6c06f2e9a9cf092cac09b59cc280ed919ded2677
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-11-20 18:01:20 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import {Route,Switch, Redirect} from 'react-router-dom';
-import './App.css';
-import Results from './results';
-import UserData from './userData';
-import Home from './home';
-import Navbar from './navbar';
-import NotFound from './notFound';
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import Results from "./results";
+import UserData from "./userData";
+import Home from "./home";
+import Navbar from "./navbar";
+import NotFound from "./notFound";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <Switch>
-      <Route path="/results" component={Results}/>
-      <Route path="/user" component={UserData}/>
-      <Route path="/not-found" component={NotFound}/>
-      <Redirect from="/home" to="/"/>
-      <Route path="/" exact component={Home}/>
-      <Redirect to="not-found"/>
+        <Route path="/results" component={Results} />
+        <Route path="/user" component={UserData} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/home" to="/" />
+        <Route path="/" exact component={Home} />
+        <Redirect to="not-found" />
       </Switch>
     </div>
   );
