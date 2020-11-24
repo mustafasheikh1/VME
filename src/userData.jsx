c1ec86a3de0e146f194518cf0700d6a90718a589
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-11-25 03:56:02 +0500

    Upload picture page completed

--- src/userData.jsx
+++ src/userData.jsx
@@ -1,64 +1,87 @@
 import React, { Component } from "react";
 import image from "./imageicon.png";
-
+import ReactDOM from "react-dom";
 class UserData extends Component {
   state = {
     imageIcon: image,
   };
 
   handleChange = (e) => {
-    // const imageIcon = e.currentTarget.files[0];
-    // this.setState({ imageIcon });
     this.setState({ imageIcon: URL.createObjectURL(e.target.files[0]) });
     console.log(e.currentTarget.files[0]);
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if (this.state.imageIcon === image) {
+      this.handleError();
+      return;
+    }
     this.props.history.push("./results");
   };
 
   render() {
     return (
       <React.Fragment>
-        <div className="row">
-          <div className="col-sm-12">
-            <h2 style={{ paddingTop: "20px", paddingLeft: "30px" }}>
-              <strong>Please upload your picture.</strong>
+        <div className="row" style={{ paddingTop: "70px" }}>
+          <div className="col-sm-5">
+            <h2
+              style={{
+                paddingTop: "120px",
+                float: "right",
+                paddingRight: "20px",
+              }}
+            >
+              <strong>
+                Please upload your <br></br>picture.
+              </strong>
             </h2>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-3 offset-sm-4">
-            <div className="text-center" style={{ paddingTop: "40px" }}>
-              <img
-                src={this.state.imageIcon}
-                className="rounded"
-                alt=""
-                style={{
-                  maxHeight: "150px",
-                  maxWidth: "150px",
-                  minHeight: "150px",
-                  minWidth: "150px",
-                }}
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="row" style={{ paddingTop: "20px" }}>
-          <div className="col-sm-2 offset-sm-5">
-            <input onChange={this.handleChange} type="file" alt="" />
-          </div>
-          <div className="col-sm-2">
+          <div
+            className="col-sm-4"
+            style={{
+              paddingTop: "60px",
+              borderLeft: "solid",
+              paddingLeft: "40px",
+              paddingBottom: "30px",
+            }}
+          >
+            <img
+              src={this.state.imageIcon}
+              className="rounded"
+              alt=""
+              style={{
+                maxHeight: "150px",
+                maxWidth: "150px",
+                minHeight: "150px",
+                minWidth: "150px",
+              }}
+            />
+            <input
+              style={{
+                marginLeft: "25px",
+              }}
+              onChange={this.handleChange}
+              type="file"
+              alt=""
+            />
             <button onClick={this.handleSubmit} className="btn btn-dark">
               Upload
             </button>
           </div>
         </div>
+        <div id="warning" style={{ paddingLeft: "630px" }}></div>
       </React.Fragment>
     );
   }
+  handleError = () => {
+    const element = (
+      <p>
+        <b>Note: </b>Please choose an image first
+      </p>
+    );
+    ReactDOM.render(element, document.getElementById("warning"));
+  };
 }
 
 export default UserData;
