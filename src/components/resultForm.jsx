5bb093ec61b1856ce9ae421787a36a65d1c4ed79
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-12-26 20:53:34 +0500

    Result page fully implemented

--- src/components/resultForm.jsx
+++ src/components/resultForm.jsx
@@ -14,10 +14,7 @@ class ResultForm extends Form {
   schema = {
     make: Joi.string().required().label("Make"),
     model: Joi.string().required().label("Model"),
-    year: Joi.string().required().label("Year"),
-    engine: Joi.string().required().label("Engine"),
-    fuel: Joi.string().required().label("Fuel"),
-    transmission: Joi.string().required().label("Transmission"),
+    color: Joi.string().required().label("Color"),
     licencePlate: Joi.string().required().label("Licence Plate"),
   };
 
@@ -25,10 +22,7 @@ class ResultForm extends Form {
     const data = {
       make: "Honda",
       model: "Accord",
-      year: "18",
-      fuel: "Petrol",
-      engine: "1600 cc",
-      transmission: "Manual",
+      color: "Black",
       licencePlate: "LZX-9171",
     };
     this.setState({ data });
@@ -48,7 +42,10 @@ class ResultForm extends Form {
           <b>Note: </b>
           Please confirm whether these results are correct or not.
         </p>
-        <div className="results" style={{ paddingLeft: "200px" }}>
+        <div
+          className="results"
+          style={{ paddingLeft: "200px", paddingTop: "50px" }}
+        >
           <form onSubmit={this.handleSubmit}>
             <div className="row">
               <div className="col-sm-6" style={{ borderRight: "solid" }}>
@@ -60,112 +57,41 @@ class ResultForm extends Form {
                   label="Make"
                   error={errors.make}
                 />
-                <div className="form-group ">
-                  <label htmlFor="model">
-                    <b>Model</b>
-                  </label>
-                  <input
-                    name="model"
-                    onChange={this.handleChange}
-                    value={data.model}
-                    id="model"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.model && (
-                    <div className="alert alert-danger">{errors.model}</div>
-                  )}
-                </div>
-                <div className="form-group ">
-                  <label htmlFor="year">
-                    <b>Year</b>
-                  </label>
-                  <input
-                    name="year"
-                    onChange={this.handleChange}
-                    value={data.year}
-                    id="year"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.year && (
-                    <div className="alert alert-danger">{errors.year}</div>
-                  )}
-                </div>
-                <div className="form-group ">
-                  <label htmlFor="engine">
-                    <b>Engine</b>
-                  </label>
-                  <input
-                    name="engine"
-                    onChange={this.handleChange}
-                    value={data.engine}
-                    id="engine"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.engine && (
-                    <div className="alert alert-danger">{errors.engine}</div>
-                  )}
-                </div>
-                <div className="form-group ">
-                  <label htmlFor="fuel">
-                    <b>Fuel</b>
-                  </label>
-                  <input
-                    name="fuel"
-                    onChange={this.handleChange}
-                    value={data.fuel}
-                    id="fuel"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.fuel && (
-                    <div className="alert alert-danger">{errors.fuel}</div>
-                  )}
-                </div>
-                <div className="form-group">
-                  <label htmlFor="transmission">
-                    <b>Transmission</b>
-                  </label>
-                  <input
-                    name="transmission"
-                    onChange={this.handleChange}
-                    value={data.transmission}
-                    id="transmission"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.transmission && (
-                    <div className="alert alert-danger">
-                      {errors.transmission}
-                    </div>
-                  )}
-                </div>
-                <div className="form-group ">
-                  <label htmlFor="licencePlate">
-                    <b>Licence Plate</b>
-                  </label>
-                  <input
-                    name="licencePlate"
-                    onChange={this.handleChange}
-                    value={data.licencePlate}
-                    id="licencePlate"
-                    type="text"
-                    className="form-control "
-                  />
-                  {errors.licencePlate && (
-                    <div className="alert alert-danger">
-                      {errors.licencePlate}
-                    </div>
-                  )}
+                <Input
+                  name="model"
+                  onChange={this.handleChange}
+                  value={data.model}
+                  id="model"
+                  label="Model"
+                  error={errors.model}
+                />
+                <Input
+                  name="color"
+                  onChange={this.handleChange}
+                  value={data.color}
+                  id="color"
+                  label="Color"
+                  error={errors.color}
+                />
+                <Input
+                  name="licencePlate"
+                  onChange={this.handleChange}
+                  value={data.licencePlate}
+                  id="licencePlate"
+                  label="LicensePlate"
+                  error={errors.licencePlate}
+                />
+                <div className="row">
+                  <div className="col-sm-2" style={{ paddingTop: "10px" }}>
+                    {this.renderButton("Confirm")}
+                  </div>
                 </div>
               </div>
               <div className="col-sm-6">
                 <img
                   style={{
                     width: "400px",
-                    paddingTop: "150px",
+                    paddingTop: "80px",
                     paddingLeft: "50px",
                   }}
                   src={sideIcon}
@@ -179,9 +105,6 @@ class ResultForm extends Form {
                   vehicle.
                 </p>
               </div>
-            </div>
-            <div className="row" style={{ paddingTop: "10px" }}>
-              <div className="col-sm-2">{this.renderButton("Confirm")}</div>
             </div>
           </form>
         </div>
