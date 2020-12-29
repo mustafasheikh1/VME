f936d1937d78cc12c95b1ba01ccc003c79c52b1a
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2020-12-29 19:18:34 +0500

    Table header extracted

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -3,12 +3,14 @@ import { getMovies } from "../services/fakeMovieService";
 import { paginate } from "../utils/paginate";
 import Pagination from "./common/pagination";
 import UsersTable from "./usersTable";
+import _ from "lodash";
 
 class Users extends Component {
   state = {
     movies: [],
     pageSize: 4,
     currentPage: 1,
+    sortColumn: { path: "title", order: "asc" },
   };
 
   componentDidMount() {
@@ -25,17 +27,29 @@ class Users extends Component {
     this.setState({ currentPage: page });
   };
 
+  handleSort = (sortColumn) => {
+    this.setState({ sortColumn });
+  };
+
   render() {
-    const { movies: allMovies, pageSize, currentPage } = this.state;
+    const { movies: allMovies, pageSize, currentPage, sortColumn } = this.state;
     const { length: count } = allMovies;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const sorted = _.orderBy(allMovies, [sortColumn.path], [sortColumn.order]);
+    const movies = paginate(sorted, currentPage, pageSize);
+
     return (
       <React.Fragment>
         <div className="row">
           <div className="col-sm-8">
             <p>Showing {count} movies in the database.</p>
-            <UsersTable movies={movies} onDelete={this.handleDelete} />
+            <UsersTable
+              movies={movies}
+              onDelete={this.handleDelete}
+              onSort={this.handleSort}
+              sortColumn={sortColumn}
+            />
             <Pagination
               itemsCount={count}
               pageSize={pageSize}
--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
+import TableHeader from "./common/tableHeader";
 
-const UsersTable = ({ movies, onDelete }) => {
-  return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Genre</th>
-          <th>Stock</th>
-          <th>Rate</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <button
-                onClick={() => this.onDelete(movie)}
-                className="btn btn-danger btn-sm"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
+class UsersTable extends Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genre.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    { key: "delete" },
+  ];
+  render() {
+    const { movies, onDelete, onSort, sortColumn } = this.props;
+    return (
+      <table className="table">
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => onDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 export default UsersTable;
