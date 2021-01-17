7407d1e2ce5490f1ab3106eec88a1d3ca1809801
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2021-01-17 21:46:25 +0500

    Improvements in show ad page

--- src/components/common/displayAd.jsx
+++ src/components/common/displayAd.jsx
@@ -1,23 +1,82 @@
 import React from "react";
+import makeIcon from "../../images/makeIcon2.png";
+import modelIcon from "../../images/modelIcon2.png";
+import priceIcon from "../../images/priceIcon.png";
+import licencePlateIcon from "../../images/licenceplateIcon.png";
+import colorIcon from "../../images/colorIcon.png";
 
 const DisplayAd = (props) => {
   const { data } = props.location.state.data;
-
-  const { name, price, details, img, date, time } = data;
+  console.log(data);
+  const { name, price, img, model, color, licensePlate } = data;
   return (
     <React.Fragment>
-      <div className="container">
-        <div className="col-sm-9 offset-sm-1" style={{ paddingTop: "10px" }}>
-          <div className="card mb-3">
-            <img className="card-img-top" src={img} alt="" />
+      <div className="container" style={{ paddingTop: "60px" }}>
+        <div className="row">
+          <div className="col-sm-1" style={{ paddingTop: "10px" }}>
+            <img style={{ width: "120px" }} src={makeIcon} alt="" />
+            <h4 style={{ marginLeft: "30px" }}>Brand</h4>
+          </div>
+          <div
+            className="col-sm-2"
+            style={{ paddingLeft: "60px", paddingTop: "50px" }}
+          >
+            <h4>
+              <b>{name}</b>
+            </h4>
+          </div>
+          <div className="col-sm-1">
+            <img style={{ width: "120px" }} src={modelIcon} alt="" />
+            <h4 style={{ marginLeft: "30px" }}>Model</h4>
+          </div>
+          <div
+            className="col-sm-2"
+            style={{ paddingLeft: "60px", paddingTop: "40px" }}
+          >
+            <h4>
+              <b>{model}</b>
+            </h4>
           </div>
         </div>
-        <div style={{ paddingLeft: "100px" }}>
-          <b>Name: {name}</b> <br />
-          <b>Price: {price}</b> <br />
-          <b>Posted Date: {date}</b> <br />
-          <b>Posted Time: {time}</b> <br />
-          <b>Details: {details}</b> <br />
+        <div className="row">
+          <div className="col-sm-1">
+            <img style={{ width: "120px" }} src={colorIcon} alt="" />
+            <h4 style={{ marginLeft: "30px" }}>Color</h4>
+          </div>
+          <div
+            className="col-sm-2"
+            style={{ paddingLeft: "60px", paddingTop: "40px" }}
+          >
+            <h4>
+              <b>{color}</b>
+            </h4>
+          </div>
+          <div className="col-sm-1">
+            <img style={{ width: "120px" }} src={priceIcon} alt="" />
+            <h4 style={{ marginLeft: "30px" }}>Price</h4>
+          </div>
+          <div
+            className="col-sm-2"
+            style={{ paddingLeft: "60px", paddingTop: "40px" }}
+          >
+            <h4>
+              <b>{price}</b>
+            </h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-1">
+            <img style={{ width: "120px" }} src={licencePlateIcon} alt="" />
+            <h4>Licence Plate</h4>
+          </div>
+          <div
+            className="col-sm-3"
+            style={{ paddingLeft: "60px", paddingTop: "40px" }}
+          >
+            <h4>
+              <b>{licensePlate}</b>
+            </h4>
+          </div>
         </div>
       </div>
     </React.Fragment>
