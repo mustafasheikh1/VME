bcc26963a07b314ac845fa6939c15e3921338b69
UsmanAfzl <58268898+UsmanAfzl@users.noreply.github.com>
2021-01-19 00:59:08 +0500

    Elements got from Database

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,89 +1,130 @@
 import React, { Component } from "react";
+import { getUsers } from "../services/userService";
 import hello from "../images/hello.svg";
 import "../css/dashboard.css";
 
 class DashBoard extends Component {
+  state = {
+    users: [],
+  };
+
+  async componentDidMount() {
+    const { data } = await getUsers();
+    const users = [];
+    users[0] = data[0];
+    users[1] = data[1];
+    users[2] = data[2];
+    users[3] = data[3];
+    this.setState({ users });
+  }
   render() {
+    const { users } = this.state;
     return (
       <React.Fragment>
-        <nav class="navbar">
-          <div class="nav_icon" onclick="toggleSidebar()">
-            <i class="fa fa-bars" aria-hidden="true"></i>
+        <nav className="navbar">
+          <div className="nav_icon">
+            <i className="fa fa-bars" aria-hidden="true"></i>
           </div>
-          <div class="navbar__left">
+          <div className="navbar__left">
             <h4>Dashboard</h4>
           </div>
         </nav>
 
         <main>
-          <div class="main__container">
-            <div class="main__title">
+          <div className="main__container">
+            <div className="main__title">
               <img src={hello} alt="" />
-              <div class="main__greeting">
+              <div className="main__greeting">
                 <h1>Hello there!</h1>
                 <p>Welcome to your admin dashboard</p>
               </div>
             </div>
 
-            <div class="main__cardds">
-              <div class="cardd">
+            <div className="main__cardds">
+              <div className="cardd">
                 <i
-                  class="fa fa-user-o fa-2x text-lightblue"
+                  className="fa fa-user-o fa-2x text-lightblue"
                   aria-hidden="true"
                 ></i>
-                <div class="cardd_inner">
-                  <p class="text-primary-p">Number of Users</p>
-                  <span class="font-bold text-title">578</span>
+                <div className="cardd_inner">
+                  <p className="text-primary-p">Number of Users</p>
+                  <span className="font-bold text-title">578</span>
                 </div>
               </div>
 
-              <div class="cardd">
-                <i class="fa fa-car fa-2x text-green" aria-hidden="true"></i>
-                <div class="cardd_inner">
-                  <p class="text-primary-p">Number of Cars</p>
-                  <span class="font-bold text-title">645</span>
+              <div className="cardd">
+                <i
+                  className="fa fa-car fa-2x text-green"
+                  aria-hidden="true"
+                ></i>
+                <div className="cardd_inner">
+                  <p className="text-primary-p">Number of Cars</p>
+                  <span className="font-bold text-title">645</span>
                 </div>
               </div>
             </div>
 
-            <div class="charts">
-              <div class="charts__left">
-                <div class="charts__left__title">
+            <div className="charts">
+              <div className="charts__left">
+                <div className="charts__left__title">
                   <div>
                     <h1>Daily Reports</h1>
                     <p>Cupertino, California, USA</p>
                   </div>
-                  <i class="fa fa-usd" aria-hidden="true"></i>
+                  <i className="fa fa-usd" aria-hidden="true"></i>
                 </div>
-                <div id="apex1"></div>
+                <div id="apex1">
+                  <table className="table">
+                    <thead style={{ fontSize: "20px" }}>
+                      <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                      </tr>
+                    </thead>
+                    <tbody style={{ fontSize: "15px" }}>
+                      {users.map((user) => (
+                        <tr>
+                          <td>
+                            <i className="fa fa-user-o" aria-hidden="true"></i>
+                          </td>
+                          <td>{user.name}</td>
+                          <td>{user.username}</td>
+                          <td>{user.email}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </div>
 
-              <div class="charts__right">
-                <div class="charts__right__title">
+              <div className="charts__right">
+                <div className="charts__right__title">
                   <div>
                     <h1>Stats Reports</h1>
                     <p>Cupertino, California, USA</p>
                   </div>
-                  <i class="fa fa-usd" aria-hidden="true"></i>
+                  <i className="fa fa-usd" aria-hidden="true"></i>
                 </div>
 
-                <div class="charts__right__cardds">
-                  <div class="cardd1">
+                <div className="charts__right__cardds">
+                  <div className="cardd1">
                     <h1>Income</h1>
                     <p>$75,300</p>
                   </div>
 
-                  <div class="cardd2">
+                  <div className="cardd2">
                     <h1>Sales</h1>
                     <p>$124,200</p>
                   </div>
 
-                  <div class="cardd3">
+                  <div className="cardd3">
                     <h1>Users</h1>
                     <p>3900</p>
                   </div>
 
-                  <div class="cardd4">
+                  <div className="cardd4">
                     <h1>Orders</h1>
                     <p>1881</p>
                   </div>
